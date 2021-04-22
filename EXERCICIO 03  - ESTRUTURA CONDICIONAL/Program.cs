8b43856cf325088df3c1e0445d5ac8ef710a3aaa
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-22 16:07:56 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIO 03  - ESTRUTURA CONDICIONAL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program{
+    /// -------> START / MAIN 
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    static void Main(string[] args){
+        Msg();
+        Conditional();
+    }
+    /// -------> FUNCTIONS
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    static void Msg(){
+        Console.WriteLine("Dizer se este número é negativo ou não");
+    }
+    //------------------------------------------------------------------------------------------//
+    static void Conditional(){
+        Console.Write("Digite o valor numérico inteiro: ");
+        int a = int.Parse(Console.ReadLine());
+        bool c1 = (a % 2 == 0);
+
+        if (c1 == true){
+            Console.WriteLine("Par");
+        }
+        else{
+            Console.WriteLine("impar");
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    /// -------> END
+}
